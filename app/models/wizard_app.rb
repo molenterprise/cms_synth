401728340823f615e72d44ce912ad3719797042c
Jaisse Grela <jgrela@gmail.com>
2014-07-22 12:34:29 -0300

    Merging versions

--- app/models/wizard_app.rb
+++ app/models/wizard_app.rb
@@ -1,7 +1,4 @@
 class WizardApp < ActiveRecord::Base
-<<<<<<< HEAD
-=======
   has_one :ontology
   validates :name, length: { minimum:3, maximum: 140 }
->>>>>>> 4b9f22ee6c1148ec779e4ab1efe50288b42963f2
 end
