62ee35455ec944333822d127a29693656f275678
Milena Ossorio <milena.ossorio@gmail.com>
2014-09-05 17:56:01 -0300

    configuratingWindows

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,8 +2,15 @@
   var app = angular.module('wizard', ['ui.bootstrap']);
 
   app.controller('WizardController', function(){
-    
- //   this.window = windows[0];
+  	
+  	/*var step = this;
+  	step.window = [];
+  	$http.get('../../config/windows.json').success(function(data){
+  		step.windows = data;
+  		step.window = data[0];
+  	});
+    */
+    this.window = wizard[0];
     
     this.solution = 0;
     
@@ -20,9 +27,12 @@
     		solution: this.solution
     	};
     	this.userSequence.push(step);
+    	if (this.window.type == "unique") /// nuevo
+    		document.location = "windows";
     	this.selectWindow(this.window.options[this.solution].next);
     	
     	this.solution = 0;
+    	
     	
     };
     
@@ -36,9 +46,12 @@
     
     
     this.selectWindow = function(index){
+    	if(index = -1) document.location = "addOntology.html";
     	for (i = 0; i < wizard.length; i++) {
-		    if(wizard[i].id == index){
+		    if(wizard[i].id == index){	
 		    	this.window = wizard[i];
+		    	if(wizard[i].type == "unique")	 
+		    		document.location = wizard[i].message;
 		    	break;
 		    }
 		}
@@ -94,4 +107,53 @@
     	return (!str || 0 === str.length);    	
     };
   }]);
+  
+  var wizard = [
+  	{
+  	  id: 1,
+      title: 'Ontologies',
+      type: 'radioDetail',
+      message: 'Ontologies',
+      options: [
+        {key: 0, text:"Add Ontology", next: -1},{key: 1, text:"Create Application", next: 2} 
+      ],
+      details:
+      	[
+	      {
+	      	title: '',
+	      	items: [
+	      		[{type: 'text', msg: "foaf"}, {type: 'text', msg: "http://xmlns.com/foaf/0.1/"}],
+	      		[{type: 'text', msg: "skos"}, {type: 'text', msg: "http://www.w3.org/2004/02/skos/core#"}],
+	      		[{type: 'text', msg: "swc"}, {type: 'text', msg: "http://data.semanticweb.org/ns/swc/ontology#"}]
+	      	]
+	      },
+	      {
+	      	title: '',
+	      	items: [
+	      		[{type: 'text', msg: "foaf"}, {type: 'text', msg: "http://xmlns.com/foaf/0.1/"}],
+	      		[{type: 'text', msg: "skos"}, {type: 'text', msg: "http://www.w3.org/2004/02/skos/core#"}],
+	      		[{type: 'text', msg: "swc"}, {type: 'text', msg: "http://data.semanticweb.org/ns/swc/ontology#"}]
+	      	]
+	      },
+	    ]
+    },
+    {
+  	  id: 2,
+      title: 'Add Ontology',
+      type: 'unique',
+      message: 'addOntology.html',
+      options: [
+        {key: 0, next: 3} 
+      ]
+    },
+    {
+  	  id: 3,
+      title: 'Green',
+      type: 'radio',
+      message: 'Wizard 3',
+      options: [
+        {key: 0, text:"Red", next: 1},{key: 1, text:"Blue", next: 2},{key: 2, text:"Green", next: 3} 
+      ]
+    }];
+    
 })();
